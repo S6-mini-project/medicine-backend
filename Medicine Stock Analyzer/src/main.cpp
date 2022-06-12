abc6be302bba0135e858c80208d938d46cafd649
Raj Diwakar <ard121001@gmail.com>
2022-06-12 16:20:11 +0530

    read data inc

--- Medicine Stock Analyzer/src/main.cpp
+++ Medicine Stock Analyzer/src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
+#include "HX711.h"
 
-void setup() {
-  // put your setup code here, to run once:
+#define DOUT 14
+#define CLK 12
+
+HX711 scale;
+void setup()
+{  
+   //float calibration_factor = 888;
+   Serial.begin(9600);
+   Serial.println("HX711 calibration sketch");
+   scale.begin(14,12);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  int units;
+ units = scale.get_units(), 10;
+ Serial.print(units);
+ if(units<0)
+ {
+   units=0;
+ }
+  if(units>0)
+  {
+    Serial.print(units);
+    Serial.print("grams");
+  }
 }
