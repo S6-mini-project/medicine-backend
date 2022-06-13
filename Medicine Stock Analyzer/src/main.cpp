c4278149f40bbbb151f5dd5390d0210be82b6f1b
Raj Diwakar <ard121001@gmail.com>
2022-06-13 22:51:19 +0530

    calib 1 inc

--- Medicine Stock Analyzer/src/main.cpp
+++ Medicine Stock Analyzer/src/main.cpp
@@ -5,25 +5,52 @@
 #define CLK 12
 
 HX711 scale;
+float calibration_factor=888;
+int units;
 void setup()
 {  
    //float calibration_factor = 888;
    Serial.begin(9600);
    Serial.println("HX711 calibration sketch");
    scale.begin(14,12);
+   Serial.println("Remove all wieghts");
+   Serial.println("After reading place known weights");
+   Serial.println("Press a or + to increase calibration factor");
+   Serial.println("Press z or - to decrease the calibration factor");
+
+   scale.set_scale();
+   scale.tare();//Reset scale value to 0
+
+   long zero_factor=scale.read_average();//Get baseline reading
+   Serial.println("Zero Factor:");
+   Serial.print(zero_factor);
 }
 void loop()
 {
-  int units;
+ scale.set_scale(calibration_factor);//Adjust to this calibration factor
+ Serial.print("Reading");
  units = scale.get_units(), 10;
- Serial.print(units);
- if(units<0)
+if(units<0)
  {
-   units=0;
+    units=0;
  }
-  if(units>0)
-  {
+   if(units>=0)
+   {
     Serial.print(units);
-    Serial.print("grams");
-  }
+    Serial.print(" grams");
+    Serial.println();
+    delay(5000);
+   }
+   if(Serial.available())
+   {
+    char temp= Serial.read();
+    if(temp=='a'||temp=='+')
+    {
+      calibration_factor=calibration_factor+1;
+    }
+    else if(temp=='z'||temp=='-')
+    {
+      calibration_factor=calibration_factor-1;
+    }
+   }
 }
