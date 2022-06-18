997f0f29653aac4a35ed5acf61a54748d55d9fa8
Raj Diwakar <ard121001@gmail.com>
2022-06-18 22:39:35 +0530

    work on print interval

--- Medicine Stock Analyzer/src/main.cpp
+++ Medicine Stock Analyzer/src/main.cpp
@@ -268,7 +268,7 @@ void loop() {
   display.drawString(0,10,"Jeevan");
   display.display();
   static boolean newDataReady = 0;
-  const int serialPrintInterval = 0; //increase value to slow down serial print activity
+  const int serialPrintInterval = 50; //increase value to slow down serial print activity
 
   // check for new data/start next conversion:
   if (LoadCell.update()) newDataReady = true;
