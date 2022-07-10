ef2f3765d52849054ac0f7744d948b96b1b52b43
A Raj Diwakar <86196887+jeevan221b@users.noreply.github.com>
2022-07-10 23:01:22 +0530

    Differentiation Successful

--- Medicine Stock Analyzer/test.php
+++ Medicine Stock Analyzer/test.php
@@ -3,10 +3,10 @@
 
 <?php
 
-$dbname = 'medpharma';
+$dbname = 'project_test';
 $dbuser = 'root';  
 $dbpass = ''; 
-$dbhost = '192.168.18.178:3306'; 
+$dbhost = 'localhost'; 
 
 $connect = @mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 
@@ -17,21 +17,27 @@ if(!$connect){
 
 echo "Connection Success!<br><br>";
 
-$wt= $_GET["weight"];
-$weight=sprintf("%.2f",$wt);
+
+$w= $_GET["weight"];
+$weight= substr($w,0,-1);
+$val= substr($w, -1);
+$key=(int) $val;
+// $weight=sprintf("%.2f",$wt);
+
 $timestamp = time();
 $date=date_default_timezone_set('Asia/Kolkata');
 $time=date("y-m-d h:i:s A");
 echo $time;
+echo $date;
 
-$q0 = mysqli_query($connect,"SELECT * FROM api_medicinebase WHERE m_id = 1");
+$q0 = mysqli_query($connect,"SELECT * FROM weighttest WHERE id = $key");
 if (mysqli_num_rows($q0) == 0)
 {
-    $q1 = mysqli_query($connect,"INSERT INTO api_medicinebase(m_id,medicine_weight,created_at,updated_at) VALUES (1,'$weight','$time','$time')");
+    $q1 = mysqli_query($connect,"INSERT INTO weighttest(id,weight,created,updated) VALUES ($key,'$weight','$time','$time')");
 }
  else
  {
-    $q2 = mysqli_query($connect,"UPDATE api_medicinebase SET medicine_weight='$weight',updated_at='$time' WHERE m_id = 1");
+    $q2 = mysqli_query($connect,"UPDATE weighttest SET weight='$weight',updated='$time' WHERE id = $key");
  }
 
 
